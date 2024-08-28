8ec5cda8f2ac20c80e0484c85bff81d0bb33bb45
AngelHdezDev <angeldev20340@gmail.com>
2024-08-28 11:56:22 -0600

    Updates in UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -15,29 +16,37 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+
+        $superAdminRole = Role::firstOrCreate(['name' => 'superAdmin']);
+        $bibliothecaryRole = Role::firstOrCreate(['name' => 'bibliothecary']);
+        $userRole = Role::firstOrCreate(['name' => 'user']);
+
         // Crear un usuario super administrador
-        User::create([
+        $admin = User::create([
             'name' => 'Admin User',
             'email' => 'admin@example.com',
             'password' => Hash::make('password'), 
             'role' => 'superAdmin', 
         ]);
+        $admin->assignRole($superAdminRole);
 
         // Crear un usuario bibliotecario
-        User::create([
+        $bibliothecary = User::create([
             'name' => 'Bibliothecary',
             'email' => 'bibliotecario@example.com',
             'password' => Hash::make('password'),
             'role' => 'bibliothecary',
         ]);
+        $bibliothecary->assignRole($bibliothecaryRole);
 
         // Crear un usuario normal
 
-        User::create([
+        $user = User::create([
             'name' => 'User',
             'email' => 'user@example.com',
             'password' => Hash::make('password'),
             'role' => 'user',
         ]);
+        $user->assignRole($userRole);
     }
 }
